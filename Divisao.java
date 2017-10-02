4a4c3af46c80f7601e33786866a679930789876c
farrel121 <31021522+farrel121@users.noreply.github.com>
2017-10-02 00:34:20 -0300

    Create Divisao.java

---
+++ Divisao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Divisao{
+	
+	public void fazerOperacao() {
+        	Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o primeiro numero");
+		int numUm = scanner.nextInt();
+		System.out.println("Digite o segundo numero");
+		int numDois = scanner.nextInt();
+		System.out.println(numUm/numDois);
+    	} 
+		
+}
